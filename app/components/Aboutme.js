1b98cb89d17bf4b1dbef36d00a5ff181a5377996
june.k.lara@gmail.com <june.k.lara@gmail.com>
2017-02-01 17:51:34 -0600

    fixed about me section

--- app/components/Aboutme.js
+++ app/components/Aboutme.js
@@ -23,7 +23,7 @@ export default class Name extends React.Component {
                             My real name is June Lara and my life for the past 3 months-850ish hours have been nonstop programmatic. I hope to dedicate many thousands hours more to this awesome community and into creating new technology for better world. I am a junior software engineer, a full stack developer - basically whatever fancy title best describes the skill set Liam Neeson only could wish to wield. My first encounter with the temptress that is computer science was as a student in my high school web development elective, learning HTML and CSS. However, UX design is not solely responsible for luring me into the depths of programming - for that, I thank Alan Turing and his remarkable breakthrough about computable numbers. My favorite actives involve writing code, studying the tech world, tinkering with my Raspberry Pi and patiently waiting for season 3 of Rick and Morty.
                         </p>
                         <p>
-                            Contact me via <a href="https://www.linkedin.com/in/dudenamedjune">LinkedIn</a> if you are interested in poaching me from my entrepreneurial projects.</p>
+                          Feel free to contact me if you're intersted in having me on your team, or just to chat through <a href="https://www.linkedin.com/in/dudenamedjune">LinkedIn</a>.</p>
                     </div>
                     <div className="col-xs-1"></div>
                     <div className="col-xs-3">
